d97692e39b0937b2e381fa04bb4ec4eff936ff7c
Vishal Dhawan <superdhawan@gmail.com>
2017-12-05 20:35:05 +0530

    Final Build
    With Swipe Actions (On the number view)
    Swipe Up to save contact
    down to open contacts
    left to clear number
    right to call a favorite number

--- app/src/main/java/straightforwardapps/decthree/MainActivity.java
+++ app/src/main/java/straightforwardapps/decthree/MainActivity.java
@@ -2,30 +2,77 @@ package straightforwardapps.decthree;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.TimedText;
 import android.net.Uri;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.ContactsContract;
+import android.service.carrier.CarrierMessagingService;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.security.Timestamp;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
 
     TextView num_show;
     String s;
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         num_show = (TextView) findViewById(R.id.num_show);
+        but = (Button) findViewById(R.id.a0);
+        final PackageManager manager = getPackageManager();
+
+        num_show.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeTop() {
+                Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                v.vibrate(VibrationEffect.createOneShot(100, 250));
+                Intent con = new Intent(ContactsContract.Intents.Insert.ACTION);
+                con.putExtra(ContactsContract.Intents.Insert.PHONE, num_show.getText().toString());
+                con.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                startActivity(con);
+            }
+
+            public void onSwipeRight() {
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse("tel:" + "8010882268"));//Set your frequent calling number
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                v.vibrate(VibrationEffect.createOneShot(150, 250));
+                startActivity(i);
+            }
+            public void onSwipeLeft() {
+                Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                v.vibrate(VibrationEffect.createOneShot(150, 250));
+                num_show.setText(null);
+            }
+            public void onSwipeBottom() {
+                Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                v.vibrate(VibrationEffect.createOneShot(100, 250));
+                Intent con = manager.getLaunchIntentForPackage("com.android.contacts");
+                startActivity(con);
+            }
+
+        });
     }
 
     public void callbaby(View view)
@@ -114,8 +161,6 @@ public class MainActivity extends AppCompatActivity {
             num_show.setText(lala);
         }
     }
-
-
 
 
 }
